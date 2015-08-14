32a72bc28905a3568ee8eeea0a5292eded5ebe61
Daniel Saper <dansaper@umich.edu>
2015-08-14 15:52:17 -0400

    Added install_requires to setup

--- PDBDistanceCalculator/setup.py
+++ PDBDistanceCalculator/setup.py
@@ -9,14 +9,13 @@ setup (
     description = "A program for finding the distance of atoms to isoalloxazine atoms",
     author = "Daniel Saper",
     packages = find_packages(),
+    install_requires = [
+        "numpy",
+        "biopython"
+    ],
     entry_points = {
         'console_scripts': [
             'flavinDistanceCalculator = pdbmanip.main:flavin_funct'
         ]
-    }
-       
-       
-       
-       
-       
+    }      
 )
